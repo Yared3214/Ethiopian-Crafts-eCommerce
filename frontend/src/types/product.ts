7520912be1e38b6e7b7819b507b6faf6d095adb2
Yohannes Walle <etawe1627@gmail.com>
2024-11-11 10:53:38 -1200

    Defined Product type interface

---
+++ frontend/src/types/product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+    title: string;
+    images: string[];
+    description: string;
+    materials: string[];
+    createdBy: string;
+    category: string;
+    price: number;
+    rating: number;
+    slug: string;
+}
