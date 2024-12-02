33e1f342cfedc78d7a3ad20bb8b5adf30af16797
Yohannes Walle <etawe1627@gmail.com>
2024-12-02 08:14:15 -1200

    mongodb conenction updated

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,19 +1,26 @@
-// src/config/db.ts
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import colors from 'colors';
 
-dotenv.config();
+dotenv.config(); 
 
 const connectDB = async () => {
     try {
-        const mongoURI = process.env.MONGO_URI as string; // Ensure MONGO_URI is defined
-        await mongoose.connect(mongoURI);
-        console.log(colors.america('Connected to MongoDB'));
-    } catch (error) {
-        console.error(colors.red('Failed to connect to MongoDB:'), error);
-        process.exit(1); // Exit the process with failure
+
+
+        // Mongoose connection
+        await mongoose.connect(process.env.MONGO_URI as string, {
+            serverSelectionTimeoutMS: 30000, 
+        });
+
+
+        console.log(colors.green('✅ Connected to MongoDB'));
+    } catch (error: any) {
+        console.error(colors.red('❌ Failed to connect to MongoDB:'), error.message);
+        process.exit(1); 
     }
 };
+
+mongoose.set('debug', true);
 
 export default connectDB;
