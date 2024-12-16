63cbf84076362b0e2aad0b88e573d6d08ce699fe
Yohannes Walle <etawe1627@gmail.com>
2024-12-15 12:15:53 -1200

    Introduced TracingBeam component for layout effect.

--- frontend/src/components/ui/bento-grid.tsx
+++ frontend/src/components/ui/bento-grid.tsx
@@ -26,13 +26,11 @@ export const BentoGridItem = ({
   description,
   imageSrc,
   slug,
-  header,
   icon,
 }: {
   className?: string;
   title?: string | React.ReactNode;
   description?: string | React.ReactNode;
-  header?: React.ReactNode;
   icon?: React.ReactNode;
   imageSrc?: string;
   slug?: string;
@@ -62,10 +60,10 @@ export const BentoGridItem = ({
       <Link href={`/blog/${slug}`}>
       <div className="group-hover/bento:translate-x-2 transition duration-200">
         {icon}
-        <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
+        <div className="font-sans font-bold line-clamp-1 text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
           {title}
         </div>
-        <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
+        <div className="font-sans font-normal line-clamp-2 text-neutral-600 text-xs dark:text-neutral-300">
           {description}
         </div>
       </div>
