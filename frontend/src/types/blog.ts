35d7e43a30c076a8db05c92cc276e4c927c71fb1
Yohannes Walle <etawe1627@gmail.com>
2024-12-15 12:16:43 -1200

    Defined blog-related types in src/types/blog.ts

---
+++ frontend/src/types/blog.ts
@@ -0,0 +1,16 @@
+
+// types.ts
+export interface Blog {
+    _id: string;
+    title: string;
+    description: string;
+    image: string;
+    badge : string;
+    category: string;
+    slug: string;
+    createdAt: string;
+    updatedAt: string;
+    __v: number;
+}
+
+
