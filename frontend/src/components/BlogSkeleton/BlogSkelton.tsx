912f133801e8c7e8aa98efa221e5a83e0b898e82
Yohannes Walle <etawe1627@gmail.com>
2024-12-15 12:14:24 -1200

    - Added BlogSkeleton component for loading state.

---
+++ frontend/src/components/BlogSkeleton/BlogSkelton.tsx
@@ -0,0 +1,20 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+
+export function BlogSkeleton() {
+    return (
+        <div className="w-full">
+            <div className="bg-gray-50 dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-full h-auto rounded-xl p-4 sm:p-6 border space-y-4">
+
+
+                {/* Image */}
+                <Skeleton className="h-40  w-full rounded-xl" />
+                {/* Title */}
+                <Skeleton className="h-6 w-3/4 rounded-md" />
+
+                {/* Description */}
+                <Skeleton className="h-4 w-full rounded-md" />
+            </div>
+        </div>
+    );
+}
