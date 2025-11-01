db6f8e718d7ad3be64ca714dcc711050fd297fbc
Yared3214 <yaredman0099@gmail.com>
2025-10-31 20:34:21 -0700

    added searchbar in  manage product component

--- frontend/src/components/DeleteButton/deleteButton.tsx
+++ frontend/src/components/DeleteButton/deleteButton.tsx
@@ -1,10 +1,40 @@
 import { Trash2 } from "lucide-react";
 import { Button } from "../ui/button";
+import {
+  AlertDialog,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogFooter,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogCancel,
+  AlertDialogAction,
+} from "@/components/ui/alert-dialog"
 
 const DeleteButton = ({ onClick }: { onClick: () => void }) => (
+  <AlertDialog>
+    <AlertDialogTrigger asChild>
   <Button variant="destructive" size="icon" onClick={onClick}>
     <Trash2 className="h-4 w-4" />
   </Button>
+  </AlertDialogTrigger>
+  <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete the product.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={onClick}>
+            Yes, delete
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+  </AlertDialog>
 );
 
 export default DeleteButton;
