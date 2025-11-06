764ae5de444acea213e5e0d359a2e51ece7d137d
mark133227 <etawe1627@gmail.com>
2025-11-06 23:03:45 +0300

    enhance blog creation flow with improved form validation and UX updates

--- frontend/src/components/AddBlog/addBlog.tsx
+++ frontend/src/components/AddBlog/addBlog.tsx
@@ -1,14 +1,34 @@
+"use client";
+
 import BlogForm from "../BlogForm/blogForm";
+import useBlog from "../../hooks/useblog";
+import { showToast } from "nextjs-toast-notify"; // ✅ make sure this is correctly imported
 
 export default function AddBlog() {
-    const handleAddBlog = (data: any) => {
-        // Logic to add product
-        console.log("Product added:", data);
+    const { createBlogHandler, loading, error } = useBlog();
+
+    const handleAddBlog = async (data: any) => {
+        try {
+            const result = await createBlogHandler(data);
+
+            if (result) {
+                showToast.success("Blog Created Successfully", {
+                    duration: 4000,
+                    progress: false,
+                    position: "bottom-right",
+                    transition: "slideInUp",
+                    icon: "",
+                    sound: false,
+                });
+            }
+        } catch (err: any) {
+           console.error("❌ Error adding blog:", err);
+        }
     };
 
     return (
         <div className="w-full">
-            <BlogForm onSubmit={handleAddBlog} />
+            <BlogForm onSubmit={handleAddBlog} loading={loading} error={error} />
         </div>
     );
 }
