60be27e314e7035e051f3792b3e4453ac5f12e35
mark133227 <etawe1627@gmail.com>
2025-11-06 23:12:05 +0300

    update store setup to include new slice configurations

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,10 +1,8 @@
-// src/app/store.ts
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './feature/user/userSlice';
 import productReducer from './feature/product/productSlice';
 import cartReducer from './feature/cart/cartSlice';
 import blogReducer from './feature/blog/blogSlice'
-
 
 const store = configureStore({
     reducer: {
