db3ad9ff9a3beff7bd61a1c323f277cce6b62dbc
mark133227 <etawe1627@gmail.com>
2025-11-06 23:02:42 +0300

    improve add product and image upload handling

--- frontend/src/components/AddProduct/addProduct.tsx
+++ frontend/src/components/AddProduct/addProduct.tsx
@@ -1,14 +1,38 @@
+"use client";
+
 import ProductForm from "../ProductForm/productForm";
+import useProduct from "../../hooks/useProduct"; // make sure this hook exists
+import { showToast } from "nextjs-toast-notify";
 
 export default function AddProduct() {
-    const handleAddProduct = (data: any) => {
-        // Logic to add product
-        console.log("Product added:", data);
-    };
+  const { createProductHandler, loading, error } = useProduct();
 
-    return (
-        <div className="w-full">
-            <ProductForm onSubmit={handleAddProduct} />
-        </div>
-    );
+  const handleAddProduct = async (data: any) => {
+    try {
+      console.log("the error is here", error);
+      const result = await createProductHandler(data);
+
+      if (result) {
+        showToast.success("Product Created Successfully", {
+          duration: 4000,
+          progress: false,
+          position: "bottom-right",
+          transition: "slideInUp",
+          icon: "",
+          sound: false,
+        });
+      }
+    } catch (err: any) {
+      showToast.error(err.message || "Failed to add product", {
+        duration: 4000,
+        position: "bottom-right",
+      }); console.error("❌ Error adding product:", err);
+    }
+  };
+
+  return (
+    <div className="w-full">
+      <ProductForm onSubmit={handleAddProduct} loading={loading} error={error} />
+    </div>
+  );
 }
