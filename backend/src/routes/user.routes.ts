96a4f684b1778f29abd6eb5e42635cb43f52c4f0
Yared3214 <yaredman0099@gmail.com>
2025-11-07 10:20:27 -0800

    save product controller and route added on user controller and route

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,7 +1,7 @@
 // src/routes/user.routes.ts
 import express from 'express';
 import { requireSignIn, adminMiddleware } from '../middlewares/authMiddleware';
-const { userRegister, getMyProfile, deleteMyAccount, updateMyAccount } = require("../controllers/user.controller")
+const { userRegister, getMyProfile, deleteMyAccount, updateMyAccount, toggleSavedProduct, getSavedProducts } = require("../controllers/user.controller")
 const router = express.Router();
 
 // Register a new user
@@ -10,6 +10,8 @@ router.post('/register', userRegister);
 router.get('/my-profile', requireSignIn, getMyProfile)
 router.delete('/delete', requireSignIn, deleteMyAccount);
 router.put('/update/myaccount', requireSignIn, updateMyAccount);
+router.post('/toggle-save-product/:productId', requireSignIn, toggleSavedProduct);
+router.get('/saved-products', requireSignIn, getSavedProducts);
 
 
 
