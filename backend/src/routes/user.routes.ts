f30327a3523f8bf8f8060caa0b171624f238fdfa
Yared3214 <yaredman0099@gmail.com>
2025-11-08 09:14:00 -0800

    updated user model, service, controller and route to add complete profile feature

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,7 +1,14 @@
 // src/routes/user.routes.ts
 import express from 'express';
 import { requireSignIn, adminMiddleware } from '../middlewares/authMiddleware';
-const { userRegister, getMyProfile, deleteMyAccount, updateMyAccount, toggleSavedProduct, getSavedProducts } = require("../controllers/user.controller")
+const { 
+    userRegister, 
+    getMyProfile, 
+    deleteMyAccount, 
+    updateMyAccount, 
+    toggleSavedProduct, 
+    getSavedProducts, 
+    completeProfile } = require("../controllers/user.controller")
 const router = express.Router();
 
 // Register a new user
@@ -12,6 +19,7 @@ router.delete('/delete', requireSignIn, deleteMyAccount);
 router.put('/update/myaccount', requireSignIn, updateMyAccount);
 router.post('/toggle-save-product/:productId', requireSignIn, toggleSavedProduct);
 router.get('/saved-products', requireSignIn, getSavedProducts);
+router.post('/complete-profile', requireSignIn, completeProfile)
 
 
 
