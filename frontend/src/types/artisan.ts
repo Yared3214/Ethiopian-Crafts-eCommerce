5cb088c779c32a65dc6536ad8bfce242be60e048
Yared3214 <yaredman0099@gmail.com>
2025-11-12 07:58:58 -0800

    updated user and artisan types to add status field

--- frontend/src/types/artisan.ts
+++ frontend/src/types/artisan.ts
@@ -2,19 +2,36 @@ import { Product } from '@/types/product'
 
 // types.ts
 export interface Artisan {
-    _id: string;
     fullName: string;
     description: string;
-    profilePic: string;
+    profilePic: File;
     slug: string;
-    createdAt: string;
-    updatedAt: string;
-    __v: number;
 }
 
 
 
 export interface ArtisanResponse {
-    artisan: Artisan;
+    artisan: {
+        _id: string;
+        fullName: string;
+        description: string;
+        profilePic: string;
+        slug: string;
+        createdAt: string;
+        updatedAt: string;
+        __v: number;  
+    };
     products: Product[];
+}
+
+export interface ArtisansResponse {
+    _id: string;
+    fullName: string;
+    status: 'active' | 'suspended';
+    description: string;
+    profilePic: string ;
+    slug: string;
+    createdAt: string;
+    updatedAt: string;
+    __v: number;
 }
--- frontend/src/types/user.ts
+++ frontend/src/types/user.ts
@@ -1,10 +1,14 @@
 // src/types/user.ts
 
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
+
 export interface User {
     _id: string;
+    avatar?: string | StaticImport;
     fullName: string;
     email: string;
     role: string; // e.g., "user", "admin", etc.
+    status: 'active' | 'suspended';
     savedProducts: string[]; // Array of saved product IDs
     createdAt: string; // ISO date string
     updatedAt: string; // ISO date string
