b15a37cffa21c32126e2a039cfe320614879d0a6
Yared3214 <yaredman0099@gmail.com>
2025-11-12 07:58:08 -0800

    updated store.ts to add artisan and customer reducers

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -3,11 +3,15 @@ import userReducer from './feature/user/userSlice';
 import productReducer from './feature/product/productSlice';
 import savedProductReducer from './feature/product/savedProductSlice'
 import cartReducer from './feature/cart/cartSlice';
-import blogReducer from './feature/blog/blogSlice'
+import blogReducer from './feature/blog/blogSlice';
+import artisanReducer from './feature/artisan/artisanSlice'
+import customersReducer from './feature/user/customersSlice'
 
 const store = configureStore({
     reducer: {
         user: userReducer,
+        customer: customersReducer,
+        artisan: artisanReducer,
         product: productReducer,
         savedProduct: savedProductReducer,
         cart: cartReducer,
