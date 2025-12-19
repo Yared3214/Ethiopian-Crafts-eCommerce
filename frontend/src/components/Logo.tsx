45283ee8d699bd983bd38b6e5826658e384450bf
mark133227 <etawe1627@gmail.com>
2025-12-19 21:41:55 +0300

    logo function has been separated from the dashboard

---
+++ frontend/src/components/Logo.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import Link from "next/link";
+import { motion } from "framer-motion";
+
+export const Logo = () => {
+  return (
+    <Link
+      href="#"
+      className="relative z-20 flex items-center space-x-2 py-1 text-sm font-normal text-black dark:text-white"
+    >
+      <div className="h-5 w-6 flex-shrink-0 rounded-bl-sm rounded-br-lg rounded-tl-lg rounded-tr-sm bg-black dark:bg-white" />
+      <motion.span
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        className="whitespace-pre font-medium"
+      >
+        Acet Labs
+      </motion.span>
+    </Link>
+  );
+};
+
+export const LogoIcon = () => {
+  return (
+    <Link
+      href="#"
+      className="relative z-20 flex items-center space-x-2 py-1 text-sm font-normal text-black dark:text-white"
+    >
+      <div className="h-5 w-6 flex-shrink-0 rounded-bl-sm rounded-br-lg rounded-tl-lg rounded-tr-sm bg-black dark:bg-white" />
+    </Link>
+  );
+};
